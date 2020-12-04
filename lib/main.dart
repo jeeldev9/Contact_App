d6a44d285300875e671e9f3eb31edceb1a64c581
Jeel Bhatti <jeelbhatti9@gmail.com>
2020-12-04 13:55:30 +0530

    Contact app

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Jeel Bhatti',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Contact App'),
+        ),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('images/profile.jpg'),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 10, bottom: 10),
+                  child: Text(
+                    "Jeel Bhatti",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontFamily: 'serif',
+                    ),
+                  ),
+                ),
+                Text(
+                  "Flutter Developer",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontFamily: 'Arial',
+                    letterSpacing: 5,
+                  ),
+                ),
+                SizedBox(
+                  height: 13,
+                  width: 145,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                  child: Padding(
+                    padding: EdgeInsets.all(9),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.phone,
+                          color: Colors.teal,
+                        ),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Text(
+                          "+91 91066 65871",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.teal,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 2, horizontal: 30),
+                  child: Padding(
+                    padding: EdgeInsets.all(9),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.email,
+                          color: Colors.teal,
+                        ),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Text(
+                          "jeelbhatti9@gamil.com",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.teal,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
